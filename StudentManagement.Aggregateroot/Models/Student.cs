5cb93de51101389bd09ae0ae51177ffe6b86fe41
MrSayem <sayem.cse72@gmail.com>
2025-02-06 12:43:12 +0600

    Add project files.

---
+++ StudentManagement.Aggregateroot/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Aggregateroot.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string Address { get; set; }
+        public int Roll {  get; set; }
+        public int ParentsCell { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+
+    }
+}
---
+++ StudentManagement.Repository/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagement.Aggregateroot.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Repository
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
